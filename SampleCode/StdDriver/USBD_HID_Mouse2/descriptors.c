25b30cc195e15d6febcb82a5adb543d9684b74ae
YJCHEN5 <YJCHEN5@nuvoton.com>
2025-01-15 17:57:21 +0800

    USBD_HID_Mouse2: Send NAK instead of 0 to fix host not going to sleep issue.
    Change-Id: I36fbc6a3ebe421626b6545f50c5ac110f1e3d92a

--- SampleCode/StdDriver/USBD_HID_Mouse2/descriptors.c
+++ SampleCode/StdDriver/USBD_HID_Mouse2/descriptors.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  * @file     descriptors.c
- * @brief    NUC1261 series USBD descriptor
+ * @brief    USBD descriptor file
  *
  * @note
  * @copyright SPDX-License-Identifier: Apache-2.0
--- SampleCode/StdDriver/USBD_HID_Mouse2/hid_mouse.h
+++ SampleCode/StdDriver/USBD_HID_Mouse2/hid_mouse.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * @file     hid_mouse.h
- * @brief    NUC1261 series USB HID mouse header file
+ * @brief    USBD HID mouse header file
  *
  * @note
  * @copyright SPDX-License-Identifier: Apache-2.0
